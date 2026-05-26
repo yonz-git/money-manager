21aa0e7a3835784befabc5753cc88d2fee79ca10
Priya <pitlapriya4u@gmail.com>
2026-05-26 15:49:01 +0200

    blocker in transaction file is fixed

--- db/models/Transaction.js
+++ db/models/Transaction.js
@@ -1,13 +1,15 @@
-import mongoose from "mongoose";
+import { Schema, model, models } from "mongoose";
  
 const transactionSchema = new Schema(
     {
         amount: { type: Number, required: true},
         title: { type: String, required: true},
-        catergory: { type: String, required: true},
+        category: { type: String, required: true},
         date: { type: Date, required: true},
         
     }
 );
+
+const Transaction = models.Transaction || model("Transaction", transactionSchema);
 
 export default Transaction;
