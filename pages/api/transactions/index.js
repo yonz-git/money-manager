79265c59df6bdbcd2f84a3d37bd068165c094e8e
Priya <pitlapriya4u@gmail.com>
2026-05-26 10:33:08 +0200

    Error handling for Database connection issue is done

---
+++ pages/api/transactions/index.js
@@ -0,0 +1,20 @@
+import dbConnect from "@/db/connect";
+import Transaction from "@/db/models/Transaction";
+
+export default async function handler(request, response) {
+  try {
+  await dbConnect();
+
+  if (request.method === "GET") {
+    const transactions = await Transaction.find();
+
+    response.status(200).json(transactions);
+    return;
+  }
+
+ 
+  response.status(405).json({ status: "Method not allowed." });
+}catch (error) {
+  response.status(500).json({ status: "Database error.", error: error.message });
+}
+}
