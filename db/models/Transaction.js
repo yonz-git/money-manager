a5ba56d93915a62294fad9f2881ddfe0c9d06d0b
Priya <pitlapriya4u@gmail.com>
2026-05-26 14:56:46 +0200

    Typo fixes and bracket fixes done

--- db/models/Transaction.js
+++ db/models/Transaction.js
@@ -1,13 +1,15 @@
-import mongoose from "mongoose";
+import {Schema, model } from "mongoose";
  
 const transactionSchema = new Schema(
     {
         amount: { type: Number, required: true},
         title: { type: String, required: true},
-        catergory: { type: String, required: true},
+        category: { type: String, required: true},
         date: { type: Date, required: true},
         
     }
 );
+ 
+const Transaction = model("Transaction", transactionSchema);
 
 export default Transaction;
