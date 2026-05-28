a7fc28f1cf1c1a35dfc750972c279353bec118b4
Priya <pitlapriya4u@gmail.com>
2026-05-28 16:24:37 +0200

    updated the TransactionsList component

--- Components/Transactions/TransactionsList.js
+++ Components/Transactions/TransactionsList.js
@@ -5,8 +5,8 @@ import { ListWrapper } from './transactions.styles';
 export default function TransactionsList({ transactions }) {
   return (
     <ListWrapper>
-      {transactions.map((tx) => (
-        <TransactionCard key={tx._id} transaction={tx} />
+      {transactions.map((transactionItem) => (
+        <TransactionCard key={transactionItem._id} transaction={transactionItem} />
       ))}
     </ListWrapper>
   );
