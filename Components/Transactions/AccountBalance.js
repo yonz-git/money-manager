96dd4e2144d24a0d1efc6791039c7d2e28317550
unknown <nebtiem@gmail.com>
2026-06-01 11:08:32 +0200

    Fixed the entire code back to what was meant to be seen

---
+++ Components/Transactions/AccountBalance.js
@@ -0,0 +1,35 @@
+import { BalanceCard, BalanceLabel, BalanceValue } from './transactions.styles';
+
+
+export default function AccountBalance(props) {
+  
+  
+  const transactions = props.transactions || [];
+
+  function calculateTotal(accumulator, currentTransaction) {
+    const amount = Number(currentTransaction.amount) || 0;
+    return accumulator + amount;
+  }
+
+  const totalBalance = transactions.reduce(calculateTotal, 0);
+
+  function formatCurrency(amount) {
+    const absoluteValue = Math.abs(amount).toLocaleString('de-DE', {
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+    });
+
+    if (amount < 0) {
+      return `€-${absoluteValue}`;
+    }
+    
+    return `€${absoluteValue}`;
+  }
+
+  return (
+    <BalanceCard $balance={totalBalance}>
+      <BalanceLabel>Account Balance</BalanceLabel>
+      <BalanceValue>{formatCurrency(totalBalance)}</BalanceValue>
+    </BalanceCard>
+  );
+}
--- Components/Transactions/TransactionsPage.js
+++ Components/Transactions/TransactionsPage.js
@@ -6,6 +6,7 @@ import TransactionsList from "./TransactionsList";
 import TransactionsSkeleton from "./TransactionsSkeleton";
 import TransactionsEmptyState from "./TransactionsEmptyState";
 import TransactionForm from "../TransactionsForm/TransactionsForm";
+import AccountBalance from "./AccountBalance"; 
 import { PageWrapper, Content, FormWrapper } from "./transactions.styles";
 
 async function fetcher(url) {
@@ -46,13 +47,21 @@ export default function TransactionsPage() {
     const transactionsClone = [...transactionsList];
 
     if (sortBy === "Newest") {
-      transactionsClone.sort((a, b) => b.date.localeCompare(a.date));
+      transactionsClone.sort(function(a, b) {
+        return b.date.localeCompare(a.date);
+      });
     } else if (sortBy === "Oldest") {
-      transactionsClone.sort((a, b) => a.date.localeCompare(b.date));
+      transactionsClone.sort(function(a, b) {
+        return a.date.localeCompare(b.date);
+      });
     } else if (sortBy === "AmountHigh") {
-      transactionsClone.sort((a, b) => b.amount - a.amount);
+      transactionsClone.sort(function(a, b) {
+        return b.amount - a.amount;
+      });
     } else if (sortBy === "AmountLow") {
-      transactionsClone.sort((a, b) => a.amount - b.amount);
+      transactionsClone.sort(function(a, b) {
+        return a.amount - b.amount;
+      });
     }
 
     return transactionsClone;
@@ -87,10 +96,13 @@ export default function TransactionsPage() {
           </FormWrapper>
         )}
 
+      
+        <AccountBalance transactions={transactionsList} />
+
         <TransactionsControls sortBy={sortBy} setSortBy={setSortBy} />
 
-        {error && <p>Could not load transactions. Please try again.</p>}
-
+  
+        {error && <p className="text-red-500 text-center">Could not load transactions. Please try again later.</p>}
         {isLoading ? (
           <TransactionsSkeleton />
         ) : shouldShowEmptyState ? (
