eb1731822e0f129aa8fefe02e78164a369e7057b
Priya <pitlapriya4u@gmail.com>
2026-06-05 16:52:32 +0200

    fix: remove try/catch from fetcher to preserve original HTTP error

--- pages/_app.js
+++ pages/_app.js
@@ -2,13 +2,9 @@ import GlobalStyle from "../styles";
 import { SWRConfig } from "swr";
 
 async function fetcher(url) {
-  try {
-    const response = await fetch(url);
-    if (!response.ok) throw new Error("Network error");
-    return response.json();
-  } catch (error) {
-    throw new Error("Failed to load data. Please try again.");
-  }
+  const response = await fetch(url);
+  if (!response.ok) throw new Error("Network error");
+  return response.json();
 }
 
 export default function App({ Component, pageProps }) {
